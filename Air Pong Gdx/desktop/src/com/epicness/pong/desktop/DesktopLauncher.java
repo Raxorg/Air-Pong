6072204fe621fb7261acd12378138f6524a7eaed
Lucho <lrdolucho@gmail.com>
2016-06-07 03:16:07 -0400

    Project base

---
+++ Air Pong Gdx/desktop/src/com/epicness/pong/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.epicness.pong.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.epicness.pong.AirPong;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AirPong(), config);
+	}
+}
